a76d64e4e258d05d1c15f3b4ee20b0d7ed28fe9b
Andrew Schineller <schineaj23@pwcs-edu.org>
2021-02-10 22:00:53 +0000

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,12 +21,14 @@ fileid="1TAe2Ywms1aM5L0wOyTt-hR3PHUNJlYne"
 filename="project.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+7z x project.zip
 
 # Create c++ file from v5blocks
 # credit to @pbchase
 echo "Creating c++ file from v5blocks file"
 cat $1 | jq -r '. | .cpp' | perl -pe "s/\\\n/\n/g;" > main.cpp
-mv main.cpp src/main.cpp
+mv main.cpp project/src/main.cpp
 
 echo "Building project..."
-make src/main.cpp
+cd project
+make
